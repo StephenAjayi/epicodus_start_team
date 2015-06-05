8e79dc5baf723ef7e95c80f6dc32f69bd7343826
stephen <lonelytreesl@yahoo.com>
2015-06-05 16:33:14 +0000

    add spec and code for Team.all() to make it pass, all specs passing.

--- lib/team.rb
+++ lib/team.rb
@@ -1,7 +1,13 @@
 class Team
+  @@all_teams = []
   
   attr_reader(:name)
+  
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+  end
+  
+  define_singleton_method(:all) do 
+    @@all_teams
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -2,10 +2,16 @@ require('rspec')
 require('team')
 
 describe(Team) do 
-  describe('name') do 
+  describe('#name') do 
     it('returns the name of a team') do 
       test_team =Team.new(:name => "Blizzard")
       expect(test_team.name()).to(eq("Blizzard"))
+    end
+  end
+  
+  describe('.all') do 
+    it('starts out empty at first') do 
+      expect(Team.all()).to(eq([]))
     end
   end
 end
